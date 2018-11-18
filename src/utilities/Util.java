aeda1b43b23f962894842a8cd74b27e3f60a1cf3
vinsmos <44977215+vinsmos@users.noreply.github.com>
2018-11-18 21:49:59 +0100

    Utilities functions for the test execution

--- src/utilities/Util.java
+++ src/utilities/Util.java
@@ -17,87 +17,87 @@ import org.openqa.selenium.support.ui.Select;
 * @version 1.0
 */
 public class Util {
-	public JavascriptExecutor js;
+    public JavascriptExecutor js;
 	
-	public List<Integer> setPriceslist(List<Integer> priceList, String[] prices) {
-		for (String price: prices) {           
+    public List<Integer> setPriceslist(List<Integer> priceList, String[] prices) {
+	    for (String price: prices) {           
 	        priceList.add(Integer.parseInt(price.replaceAll("\\D+","")));
 	    }
-		return priceList;
-	}
+	    return priceList;
+    }
 	
-	public List<Integer> setYearsList(List<Integer> yearList, String[] years) {
-		for (String year: years) {           
-	        if(year.indexOf("•")!=-1) {
-	        	year = year.replaceAll("\\r|\\n", "");
-	        	year = year.split("•")[1];
-	        	yearList.add(Integer.parseInt(year.split("/")[1]));
-	        }
-	    }
-		return yearList;
-	}
+    public List<Integer> setYearsList(List<Integer> yearList, String[] years) {
+        for (String year: years) {           
+            if(year.indexOf("•")!=-1) {
+                year = year.replaceAll("\\r|\\n", "");
+                year = year.split("•")[1];
+                yearList.add(Integer.parseInt(year.split("/")[1]));
+            }
+        }
+        return yearList;
+    }
 	
-	public void clickElement(String selector, WebDriver driver) {
-		WebElement filter = driver.findElement(By.cssSelector(selector));
-		filter.click();
-	}
+    public void clickElement(String selector, WebDriver driver) {
+        WebElement filter = driver.findElement(By.cssSelector(selector));
+        filter.click();
+    }
 
-	public boolean elementIsPresent(String selector, WebDriver driver) {
-		boolean isPresent = true;
-		List<WebElement> elements = driver.findElements(By.cssSelector(selector));
-		if (elements.size() == 0) {
-			isPresent = false;
-		}
-	    return isPresent;
-	}
+    public boolean elementIsPresent(String selector, WebDriver driver) {
+        boolean isPresent = true;
+        List<WebElement> elements = driver.findElements(By.cssSelector(selector));
+        if (elements.size() == 0) {
+            isPresent = false;
+        }
+        return isPresent;
+    }
 	
-	public void setPaginationFilter(String selector, WebDriver driver) {
-		Select pagination = new Select(driver.findElement(By.cssSelector(selector)));
-		int selectOptions = pagination.getOptions().size();
-		pagination.selectByIndex(selectOptions - 1);		
-	}
+    public void setPaginationFilter(String selector, WebDriver driver) {
+        Select pagination = new Select(driver.findElement(By.cssSelector(selector)));
+        int selectOptions = pagination.getOptions().size();
+        pagination.selectByIndex(selectOptions - 1);		
+    }
 	
-	public Integer getSearchResultItemsNumber(String selector, WebDriver driver) {
-		String total = driver.findElement(By.cssSelector(selector)).getText();
-		Integer totalItems = Integer.parseInt(total.replaceAll("[^0-9]", ""));
-		return totalItems;
-	}
+    public Integer getSearchResultItemsNumber(String selector, WebDriver driver) {
+        String total = driver.findElement(By.cssSelector(selector)).getText();
+        Integer totalItems = Integer.parseInt(total.replaceAll("[^0-9]", ""));
+        return totalItems;
+    }
 	
-	public String[] executeScript(String selector, WebDriver driver) {
-		js = (JavascriptExecutor)driver;
-		String result = js.executeScript("return Array.from(document.querySelectorAll('" + selector + "')).map(i => i.innerText);").toString();
-		return parseData(result);
-	}
+    public String[] executeScript(String selector, WebDriver driver) {
+        js = (JavascriptExecutor)driver;
+        String result = js.executeScript("return Array.from(document.querySelectorAll('" + selector + "')).map(i => i.innerText);").toString();
+        return parseData(result);
+    }
 
-	public String[] parseData(String data) {
-		String[] dataParsed = null;
-		data = data.replaceAll("\\[", "");
-		dataParsed = data.split(",");
-		return dataParsed;
-	}
+    public String[] parseData(String data) {
+        String[] dataParsed = null;
+        data = data.replaceAll("\\[", "");
+        dataParsed = data.split(",");
+        return dataParsed;
+    }
 	
-	public boolean checkValueLessThan(List<Integer> list, Integer value) {
-		boolean valueLessThan = false;
+    public boolean checkValueLessThan(List<Integer> list, Integer value) {
+        boolean valueLessThan = false;
         for (Integer data : list ) {
-        	if (Integer.valueOf(data) < Integer.valueOf(value)) {
-        		valueLessThan = true;
-        		break;
-        	}
+            if (Integer.valueOf(data) < Integer.valueOf(value)) {
+                valueLessThan = true;
+                break;
+            }
         } 		
-		return valueLessThan;
-	}
+        return valueLessThan;
+    }
 	
-	public List<Integer> copyList(List<Integer> sourceList) {
-		List<Integer> destinationlist = new ArrayList<Integer>();
-		for (Integer data : sourceList ) {
-			destinationlist.add(data);
-		}
-		return destinationlist;
-	}
+    public List<Integer> copyList(List<Integer> sourceList) {
+        List<Integer> destinationlist = new ArrayList<Integer>();
+        for (Integer data : sourceList ) {
+            destinationlist.add(data);
+        }
+        return destinationlist;
+    }
 	
-	public void checkPopUp(String selector, WebDriver driver) {
-    	if (elementIsPresent(selector, driver)) {
-    		clickElement(selector, driver);
-    	}		
-	}
+    public void checkPopUp(String selector, WebDriver driver) {
+        if (elementIsPresent(selector, driver)) {
+            clickElement(selector, driver);
+        }		
+    }
 }
